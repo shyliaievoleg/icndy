da4c85263d209fcc39a24328a313fcd3f38d4723
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2018-12-11 11:27:38 +0300

    Удалил лишние файлы

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,1 @@
-// Artworks gallery slider
-$(document).ready(function () {
-  $("#js_gallery").owlCarousel({
-    loop: true,
-    nav: true,
-    navText: ["Предыдущий", "Следующий"],
-    dots: false,
-    autoWidth: true,
-    items: 1,
-    lazyLoad: true,
-    animateOut: 'fadeOutDown',
-    animateIn: 'fadeInDown'
-  });
-});
+
